a89d6cb0f6f31c7447db6d7c72c013965e73996f
kamil.gaborek <kamil.gaborek@gmail.com>
2021-10-01 16:16:23 +0200

    created twitter-to-kafka-service

---
+++ twitter-to-kafka-service/src/test/java/com/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.microservice.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTest {
+
+   /* @Test
+    public void contextLoad(){}*/
+}
